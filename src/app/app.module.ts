e83a6527d8ba2fd2ddd67ad262f5a015798ccf34
henrywicaksono12 <c14180014@john.petra.ac.id>
2020-04-29 07:51:41 +0700

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ResultComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }

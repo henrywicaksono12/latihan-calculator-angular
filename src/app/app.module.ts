b5c246170631dacd52c9ee01a1b9adca5145e294
henrywicaksono12 <c14180014@john.petra.ac.id>
2020-04-29 08:02:53 +0700

    Calculator Buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,14 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ResultComponent } from './result/result.component';
 
+import { Routes, RouterModule } from '@angular/router';
+
+const ROUTES:Routes = [
+  { path:'result', component : ResultComponent}
+]
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(ROUTES) ],
   declarations: [ AppComponent, HelloComponent, ResultComponent ],
   bootstrap:    [ AppComponent ]
 })

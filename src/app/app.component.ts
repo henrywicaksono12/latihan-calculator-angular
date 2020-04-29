0deed73b0b4266d51babeb833399ccf86159cd0c
henrywicaksono12 <c14180014@john.petra.ac.id>
2020-04-29 08:18:59 +0700

    Commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,37 @@ import { Router } from '@angular/router'
 
 export class AppComponent  {
   name = 'Angular';
+  inputText = "";
+  bil1 = 0;
+  bil2 = 0;
+  op = "";
+  res = 0;
   
   constructor(private router : Router) {}
   
   result(){
+    this.bil1 = parseInt(this.inputText[0]);
+    this.op = this.inputText[2];
+    this.bil2 = parseInt(this.inputText[4]);
+
+    if(this.op == "+")
+    {
+      this.res = this.bil1+this.bil2;
+    }
+    else if(this.op == "-")
+    {
+      this.res = this.bil1-this.bil2;
+    }
+    else if(this.op == "*")
+    {
+      this.res = this.bil1*this.bil2;
+    }
+
     this.router.navigate(['/result']);
+  }
+
+  getResult()
+  {
+    return this.res;
   }
 }
--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-result',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultComponent implements OnInit {
 
-  constructor() { }
+  res = 0;
+
+  constructor(private appComponent:AppComponent) { }
 
   ngOnInit() {
+    this.res = this.appComponent.getResult();
   }
 
 }
